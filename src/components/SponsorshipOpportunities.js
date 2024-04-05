5cbdc4a3698fa889be8e85ee4f1b908007e196d9
fredgeorge0311 <george.fred0311@gmail.com>
2024-04-04 22:07:16 -0700

    added sponsorship page

--- src/components/SponsorshipOpportunities.js
+++ src/components/SponsorshipOpportunities.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../components/CSS/home.css";
 
-import kidsTeensTennisImage from "../assets/kidsTeens.png";
-import kidsBackImage from "../assets/Fotolia_52285910_XS_cropped.png";
+import SponsorManImage from "../assets/sponsorMan.png";
 import Login from "./Login";
 
 const SponsorshipOppotunities = () => {
@@ -49,81 +48,79 @@ const SponsorshipOppotunities = () => {
             </p>
 
             <div className="kidsTeensTennis__block">
-              <div className="kidsTeensTennisBlock_side">
-                <div className="kidsTeensSide__container">
-                  <div className="kidsTeensSide__round">
+              <div className="SponsorshipOppBlock_side">
+                <div className="SponsorshipOppSide__container">
+                  <div className="SponsorshipOppSide__round">
                     <div />
                   </div>
-                  <p>
-                    Tennis FairPlay Offers a{" "}
-                    <a
-                      href="/category/junior-stories"
-                      className="aTag__setting"
-                    >
-                      Sponsor a Junior
-                    </a>{" "}
-                    Program to Award an{" "}
-                    <span style={{ color: "#27850E" }}>Annual Grant</span> to a
-                    Junior Player Chosen by{" "}
-                    <a href="/members" className="aTag__setting">
-                      Tennis FairPlay Members
-                    </a>
-                    !
-                  </p>
+                  <div className="SponsorshipSide__paragraph">
+                    <p className="SponsorshipSidePara__title">
+                      Sponsorship Opportunities:
+                    </p>
+                    <div className="SponsorshipSidePara__details">
+                      <p>- League Sponsors</p>
+                      <p>- Sponsor Page Advertisement</p>
+                      <p>- Member Benefit Sponsor</p>
+                      <p>- Special Event Sponsor</p>
+                      <p>- Exclusive Category Sponsor</p>
+                      <p>- Junior Sponsor</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="kidsTeensTennisBlock_side">
-                <div className="kidsTeensSide__container">
-                  <div className="kidsTeensSide__round">
+              <div className="SponsorshipOppBlock_side">
+                <div className="SponsorshipOppSide__container">
+                  <div className="SponsorshipOppSide__round">
                     <div />
                   </div>
-                  <a href="/category/junior-stories">
-                    <img
-                      src={kidsBackImage}
-                      className="kidsBackImage__setting"
-                    />
-                  </a>
+                  <div className="SponsorshipSide__paragraph">
+                    <p className="SponsorshipSidePara__title">Features:</p>
+                    <div className="SponsorshipSidePara__details">
+                      <p>- Visibility on the Sponsor Page</p>
+                      <p>- Visibility on the Home Page</p>
+                      <p>- Corporate Logo on the Website</p>
+                      <p>- Corporate Logo on the Tennis Apparel</p>
+                      <p>
+                        - Corporate Logo on Tennis FairPlay's Promotional
+                        Materials/Flyers
+                      </p>
+                      <p>- Reward Gift Bags & Monthly Draw Participation</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="SponsorshipOppBlock_side">
+                <div className="SponsorshipOppSide__container">
+                  <div className="SponsorshipOppSide__round">
+                    <div />
+                  </div>
+                  <div className="SponsorshipSide__paragraph">
+                    <p className="SponsorshipSidePara__title">
+                      Additional Opportunities:
+                    </p>
+                    <div className="SponsorshipSidePara__details">
+                      <p>- Non-Monetary Contributions & Collaborations</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <p className="SeniorsTennis__paragraph">
-              Tennis BC is a leader and strong advocate for Youth Tennis. Please
-              visit{" "}
-              <a
-                href="https://www.tennisbc.org/"
-                className="aTag__setting"
-                target="_block"
-              >
-                www.tennisbc.org
-              </a>{" "}
-              for more information. Under the governance of Tennis Canada,
-              certification and courses are offered to ensure the quality of
-              instruction and to maintain an education standard of excellence.
-              Please visit{" "}
-              <a
-                href="https://www.tenniscanada.com/"
-                className="aTag__setting"
-                target="_block"
-              >
-                www.tenniscanada.com
-              </a>{" "}
-              for more information. The North Vancouver Tennis Centre is
-              designated as an official Tennis Development Centre where all
-              programs are based on the Tennis Long-Term Athlete Development
-              Path designed by Wayne Elderton, Tennis Director and Level 4
-              Coach.
-            </p>
-            <p className="SeniorsTennis__paragraph">
-              The International Tennis Federation (ITF), organizes tournaments
-              for Juniors ranging 12-18.
-            </p>
-            <p className="SeniorsTennis__paragraph">
-              From the School Systems, programs and funding from the municipal,
-              provincial and federal government bodies, the local Recreational
-              Centres, the Private Tennis Facilities & Clubs, Associations &
-              Foundations for kids Tennis is a growing sport promoted and
-              encouraged as a healthy lifestyle choice.
-            </p>
+            <div className="SponsorOpp__bottomContainer">
+              <div className="SponsorOpp__bottomLeft">
+                <p className="SeniorsTennis__paragraph sponsor__space">
+                  For programs and rates please fill out the form below and
+                  submit your interest in receiving additional information on
+                  Tennis FairPlay’s Sponsorship Opportunities
+                </p>
+                <p className="SeniorsTennis__paragraph sponsor__space">
+                  [easy_contact_forms fid=1]
+                </p>
+                <p className="SeniorsTennis__paragraph sponsor__space">
+                  <strong>Thank you for your participation!</strong>
+                </p>
+              </div>
+              <img src={SponsorManImage} />
+            </div>
           </div>
         </div>
         <Login />
