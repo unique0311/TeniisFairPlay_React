9d9f5824d08f2767cb470825aa020cda425692a4
fredgeorge0311 <george.fred0311@gmail.com>
2024-04-06 10:50:54 -0700

    junior added

--- src/components/JuniorMember.js
+++ src/components/JuniorMember.js
@@ -1,15 +1,36 @@
 import React from "react";
 
 import juniorMemberImage from "../assets/ying.png";
+import heartBlankImage from "../assets/heart_blank.png";
+import commentImage from "../assets/comment.png";
 
 const JuniorMember = () => {
   return (
     <div className="juniorMembers__container">
-      <img src={juniorMemberImage} className="juniorMemberImage__setting" />
+      <div className="juniorMembers__imageContainer">
+        <img src={juniorMemberImage} className="juniorMemberImage__setting" />
+        <div className="memberImage__hoverSetting">+</div>
+      </div>
+
       <p className="juniorMembers__name">ying</p>
       <p className="juniorMembers__address">
         sylvieq, May 1, 2014, Junior Stories,
       </p>
+      <div className="juniorMembers__message">
+        <p>0</p>
+        <img src={commentImage} />
+      </div>
+      <p className="juniorMembers__details">
+        15 years of age, Tennis Junior. Striving to prepare for acceptance in a
+        Junior Program at a University level. Very...
+      </p>
+      <div className="juniorMembers__bottom">
+        <div className="juniorMembers__likes">
+          <img src={heartBlankImage} />
+          <p>3 likes</p>
+        </div>
+        <p className="juniorMembers__bottomReadMore">Read more</p>
+      </div>
     </div>
   );
 };
